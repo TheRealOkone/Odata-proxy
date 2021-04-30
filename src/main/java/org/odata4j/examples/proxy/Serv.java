40150f40943e42caba8af26ef570d10d653040df
Your Name <you@example.com>
2021-04-30 15:31:58 +0300

    Threads

--- src/main/java/org/odata4j/examples/proxy/Serv.java
+++ src/main/java/org/odata4j/examples/proxy/Serv.java
@@ -6,27 +6,53 @@ import org.odata4j.examples.visual.Viewform;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.*;
+
+
 @Service
 public class Serv  {
 
     public Serv(@Value("${spring.datasource.password}") String password,
                 @Value("${spring.datasource.username}") String username,
                 @Value("${spring.datasource.url}") String url) {
-        int i = 0;
-        Viewform form = new Viewform();
-        while(form.getQuery() == null){
-            System.out.print("");
+
+
+
+        Thread t = Thread.currentThread();
+
+        FutureTask<Viewform> task = new FutureTask<Viewform>(new Callable<Viewform>() {
+            public Viewform call() {
+                Viewform form = new Viewform();
+                while(form.getQuery() == null){
+                    System.out.print("");
+                }
+                return form;
+            }});
+        Thread w = new Thread(task);
+        w.start();
+
+
+
+        Viewform finalform = null;
+        try {
+            finalform = task.get();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
         }
-        if(password == null){
-            password = form.getPassword();
+
+        if(finalform != null) {
+
+            if (password == null) {
+                password = finalform.getPassword();
+            }
+            if (username == null) {
+                username = finalform.getUser();
+            }
+            if (url == null) {
+                url = finalform.getDatabase();
+            }
         }
-        if(username == null){
-            username = form.getUser();
-        }
-        if(url == null){
-            url = form.getDatabase();
-        }
-        RoundtripExample example = new RoundtripExample(password,username,url,form.getQuery(),form.getUser());
+        RoundtripExample example = new RoundtripExample(password,username,url,finalform.getQuery(),finalform.getUser());
         example.run();
         try {
             Thread.sleep(3000);
@@ -35,3 +61,4 @@ public class Serv  {
         }
     }
 }
+
